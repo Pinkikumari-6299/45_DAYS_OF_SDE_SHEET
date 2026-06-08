eb7293fba185e177297d59249cf725e8a7229952
Pinkikuamri-6299 <pinkikalwar007@gmail.com>
2026-06-09 00:18:26 +0530

    Added Day 9 solutions

---
+++ Day-09/Middle_of_the_linkedlist.cpp
@@ -0,0 +1,18 @@
+// TIME COMPLEXITY :O(n/2)
+// SPACE COMPLEXITY : O(1)
+
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+    ListNode* slow=head;
+    ListNode* fast=head; 
+    while(fast!=NULL && fast->next!=NULL)   
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+        return slow;
+    }
+    
+};
---
+++ Day-09/Reverse_linkedlist.cpp
@@ -0,0 +1,18 @@
+// TIME COMPLEXITY :O(n)
+// SPACE COMPLEXITY : O(1)
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+      ListNode* temp=head;
+      ListNode* prev=NULL;
+      while(temp!=NULL) {
+        ListNode* front=temp->next;
+        temp->next=prev;
+        prev=temp;
+        temp=front;
+      } 
+       return prev; 
+    }
+};
