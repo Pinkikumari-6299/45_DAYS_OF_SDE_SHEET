9e72342bc86927649d4be986234bba36ffd71580
Pinkikuamri-6299 <pinkikalwar007@gmail.com>
2026-06-10 18:47:16 +0530

    Added Day 10 solutions

---
+++ Day-10/Delete node in a linkedlist.cpp
@@ -0,0 +1,14 @@
+// TIME COMPLEXITY :O(1)
+// SPACE COMPLEXITY : O(1)
+
+
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+         
+         node ->val= node ->next ->val;
+         node->next=node ->next ->next;
+          
+    }
+};
